97c8656e34bbc240960519d78e9c5154d15dc551
Nolan Picini <nolanpic@gmail.com>
2020-01-09 13:37:41 -0800

    Added link to marketing page via the logo

--- src/components/SongVisualization.js
+++ src/components/SongVisualization.js
@@ -23,7 +23,7 @@ const SongVisualization = props => {
     return (
         <>
             {base64Img ?
-            (<img src={`data:image/png;base64, ${base64Img}`} alt="song visualization" />)
+            (<img src={`data:image/png;base64,${base64Img}`} alt="song visualization" />)
             :
             (<p>No visualization available.</p>)}
         </>
